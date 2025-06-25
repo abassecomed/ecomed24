c8145a44b8220d56b6596a88e067f4a4678c34e3
Neha maurya <nehasudha2021@gmail.com>
2025-06-25 19:20:15 +0530

    sample-management

---
+++ models/SampleManagement.js
@@ -0,0 +1,56 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config");
+const Patient = require("./Patient");
+
+const SampleManagement = sequelize.define(
+  "SampleManagement",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    patient_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Patient,
+        key: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "CASCADE",
+    },
+    sample_type: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    test_type: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    priority: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    notes: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    added_by: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    updated_by: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: "sample_managements",
+    timestamps: true,
+    paranoid: true,
+    // deletedAt: 'deletedAt'
+  }
+);
+SampleManagement.belongsTo(Patient, {foreignKey:'patient_id', as:'patient'});
+module.exports = SampleManagement;
