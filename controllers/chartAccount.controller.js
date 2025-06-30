3eb45aca66661b9da693c4e55cd9f21c1236f4b5
Neha maurya <nehasudha2021@gmail.com>
2025-06-30 17:25:12 +0530

    complete chart account

--- controllers/chartAccount.controller.js
+++ controllers/chartAccount.controller.js
@@ -7,6 +7,13 @@ exports.getList = async (req, res) => {
     let datalimit = parseInt(req.query.limit ?? 5);
     datalimit = isNaN(datalimit) || datalimit <= 0 ? 5 : datalimit;
     const { count, rows } = await ChartAccount.findAndCountAll({
+      include: [
+        {
+          model: ChartAccount,
+          as: "parent",
+          attributes: ["id", "code", "name", "type", "parent_account"],
+        },
+      ],
       attributes: [
         "id",
         "code",
@@ -38,10 +45,19 @@ exports.getList = async (req, res) => {
   }
 };
 
-// Add new chart account
 exports.add = async (req, res) => {
   try {
     const { code, name, type, parent_account } = req.body;
+
+    if (parent_account) {
+      const parent = await ChartAccount.findByPk(parent_account);
+      if (!parent) {
+        return res.json({
+          status: 0,
+          message: "Parent account not found",
+        });
+      }
+    }
 
     const newAccount = await ChartAccount.create({
       code,
@@ -65,11 +81,19 @@ exports.add = async (req, res) => {
   }
 };
 
-// Get chart account by ID
 exports.getByID = async (req, res) => {
   try {
-    const { id } = req.params;
-    const account = await ChartAccount.findByPk(id);
+    const id = req.params.id;
+    const account = await ChartAccount.findOne({
+      where: { id },
+      include: [
+        {
+          model: ChartAccount,
+          as: "parent",
+          attributes: ["id", "code", "name", "type", "parent_account"],
+        },
+      ],
+    });
 
     if (!account) {
       return res.json({ status: 0, message: "Chart account not found" });
@@ -89,7 +113,6 @@ exports.getByID = async (req, res) => {
   }
 };
 
-// Update chart account
 exports.update = async (req, res) => {
   try {
     const { id } = req.params;
@@ -99,6 +122,17 @@ exports.update = async (req, res) => {
 
     if (!account) {
       return res.json({ status: 0, message: "Chart account not found" });
+    }
+
+    // Check if parent_account exists (if provided)
+    if (parent_account) {
+      const parent = await ChartAccount.findByPk(parent_account);
+      if (!parent) {
+        return res.json({
+          status: 0,
+          message: "Parent account not found",
+        });
+      }
     }
 
     await account.update({
@@ -123,7 +157,6 @@ exports.update = async (req, res) => {
   }
 };
 
-// Soft Delete chart account
 exports.delete = async (req, res) => {
   try {
     const { id } = req.params;
@@ -148,4 +181,3 @@ exports.delete = async (req, res) => {
     });
   }
 };
-
--- models/ChartAccount.js
+++ models/ChartAccount.js
@@ -1,43 +1,53 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config");
 
-const ChartAccount =sequelize.define(
-    "ChartAccount",
-    {
-        id:{
-            type:DataTypes.INTEGER,
-            primaryKey:true,
-            autoIncrement:true,
-        },
-        code:{
-            type:DataTypes.STRING,
-            allowNull:true,
-        },
-        name:{
-            type:DataTypes.STRING,
-            allowNull:true,
-        },
-        type:{
-            type:DataTypes.STRING,
-            allowNull:true,
-        },
-        parent_account:{
-            type:DataTypes.STRING,
-            allowNull:true,
-        },
-         added_by: {
+const ChartAccount = sequelize.define(
+  "ChartAccount",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    code: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    parent_account: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    added_by: {
       type: DataTypes.INTEGER,
       allowNull: true,
     },
     updated_by: {
       type: DataTypes.INTEGER,
       allowNull: true,
-    }
     },
-    {
-        tableName:"chart_accounts",
-        timestamps:true,
-        paranoid:true,
-    }
+  },
+  {
+    tableName: "chart_accounts",
+    timestamps: true,
+    paranoid: true,
+  }
 );
-module.exports=ChartAccount;
+
+ChartAccount.hasMany(ChartAccount, {
+  foreignKey: "parent_account",
+  as: "children",
+});
+
+ChartAccount.belongsTo(ChartAccount, {
+  foreignKey: "parent_account",
+  as: "parent",
+});
+module.exports = ChartAccount;
