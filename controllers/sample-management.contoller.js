f1cc0bb546298803dc4d327af7c422dd324e4136
Neha maurya <nehasudha2021@gmail.com>
2025-07-06 15:56:32 +0530

    lab test (order and sample management)

--- controllers/sample-management.contoller.js
+++ controllers/sample-management.contoller.js
@@ -2,6 +2,10 @@ const SampleManagement = require("../models/SampleManagement");
 const SampleStatusLog = require("../models/SampleStatusLog");
 const Patient = require("../models/Patient");
 const User = require("../models/User");
+var TestRequests = require("../models/TestRequests");
+var PatientLogs = require("../models/PatientLogs");
+var TestItems = require("../models/TestItems");
+const BASEURL = process.env.SITE_URL;
 
 SampleManagement.belongsTo(User, {
   as: "addedby_details",
@@ -195,5 +199,147 @@ exports.status = async (req, res) => {
       message: "Status update failed.",
       error: error.message,
     });
+  }
+};
+
+exports.addLabTest = async (req, res) => {
+  try {
+    let getData = [],
+      results;
+    const reports = req.body.reports;
+
+    const PatientModal = await Patient.findOne({
+      where: { id: req.body.patient_id },
+    });
+
+    const TestRequestsModal = await TestRequests.create({
+      patient_id: req.body.patient_id,
+      org_id: req.org_id,
+      type: "lab",
+      advice: req.body.advice,
+      reports: reports,
+      status: 0,
+      added_by: req.userId,
+    });
+
+    if (!TestRequestsModal) {
+      return res.json({ status: 0, message: "error" });
+    }
+
+    for (const report of reports) {
+      await TestItems.create({
+        patient_id: req.body.patient_id,
+        org_id: req.org_id,
+        request_id: TestRequestsModal.id,
+        test_id: report.id,
+        name: report.name,
+        type: report.type,
+        price: report.price,
+        status: 1,
+        added_by: req.userId,
+      });
+    }
+
+    await PatientLogs.create({
+      patient_id: TestRequestsModal.patient_id,
+      org_id: req.org_id,
+      description: "New Lab Request has been added ",
+      type: "lab",
+      action: "add",
+      relation_id: TestRequestsModal.id,
+      status: 1,
+      added_by: req.userId,
+    });
+
+    res.json({
+      status: 1,
+      message: "Lab test added successfully",
+      data: TestRequestsModal,
+    });
+  } catch (error) {
+    console.error("Error in addLabTest:", error);
+    res.status(500).json({ status: 0, message: "Server error", error });
+  }
+};
+
+exports.getLabTestList = async (req, res) => {
+  try {
+    const offset = parseInt(req.query.offset) || 0;
+    const limit = parseInt(req.query.limit) || 20;
+
+    const labTests = await TestRequests.findAndCountAll({
+      where: {
+        org_id: req.org_id,
+        type: "lab",
+      },
+      order: [["id", "DESC"]],
+      offset: offset,
+      limit: limit,
+      include: [
+        {
+          model: Patient,
+          as: "patient",
+          attributes: ["id", "name", "email"],
+        },
+        {
+          model: TestItems,
+          as: "test_items",
+          attributes: ["id", "name", "type", "price", "status"],
+        },
+      ],
+    });
+
+    res.json({
+      status: 1,
+      message: "Lab Test List fetched successfully",
+      data: labTests.rows,
+      total: labTests.count,
+    });
+  } catch (error) {
+    console.error("Error in getLabTestList:", error);
+    res.status(500).json({ status: 0, message: "Server error", error });
+  }
+};
+
+exports.getLabTestByID = async (req, res) => {
+  try {
+    const request_id = req.params.id;
+
+    if (!request_id) {
+      return res.status(400).json({ status: 0, message: "Invalid Request ID" });
+    }
+
+    const testRequest = await TestRequests.findOne({
+      where: {
+        id: request_id,
+        type: "lab",
+      },
+      include: [
+        {
+          model: Patient,
+          as: "patient",
+          attributes: ["id", "name", "email"],
+        },
+        {
+          model: TestItems,
+          as: "test_items",
+          attributes: ["id", "name", "type", "price", "status"],
+        },
+      ],
+    });
+
+    if (!testRequest) {
+      return res.json({ status: 0, message: "No Lab Test found with this ID" });
+    }
+
+    res.json({
+      status: 1,
+      message: "Lab Test fetched successfully",
+      data: testRequest,
+      url: BASEURL + "/uploads/invoicefile/",
+    });
+  } catch (error) {
+    console.error("Error in getLabTestByID:", error);
+    res.status(500).json({ status: 0, message: "Server error", error });
   }
 };
--- routes/sample-management.routes.js
+++ routes/sample-management.routes.js
@@ -10,5 +10,8 @@ router.post("/add", VerifyToken, SampleManagementController.add);
 router.patch("/update/:id", VerifyToken, SampleManagementController.update);
 router.delete("/delete/:id", VerifyToken, SampleManagementController.delete);
 router.patch("/status/:id", VerifyToken, SampleManagementController.status);
+router.post("/lab-test",VerifyToken,SampleManagementController.addLabTest);
+router.get("/lab-test", VerifyToken, SampleManagementController.getLabTestList);
+router.get("/lab-test/by-id/:id", SampleManagementController.getLabTestByID)
 
 module.exports = router;
